c926451476c9cbbdf8d000e3d01663aa1e76b751
adamgdm <116694569+adamgdm@users.noreply.github.com>
2023-12-08 17:50:45 +0100

    ex00, Module 2

--- Module 2/ex00/Fixed.cpp
+++ Module 2/ex00/Fixed.cpp
@@ -36,20 +36,3 @@ void Fixed::setRawBits(int const raw)
     std::cout << "\033[33msetRawBits member function called\033[37m" << std::endl;
     this->nbr = raw;
 }
-
-Default constructor called
-Copy constructor called
-Copy assignment operator called // <-- This line may be missing depending on your implementation
-getRawBits member function called
-Default constructor called
-Copy assignment operator called
-getRawBits member function called
-getRawBits member function called
-0
-getRawBits member function called
-0
-getRawBits member function called
-0
-Destructor called
-Destructor called
-Destructor called
