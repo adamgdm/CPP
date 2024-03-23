3119e0f917b638214960d90afd2e824c026037e4
Adam Goujdami <ad.goujdami@gmail.com>
2024-03-23 17:32:30 +0000

    Began Module 7

---
+++ Module 7/ex00/whatever.hpp
@@ -0,0 +1,13 @@
+#ifndef WHATVER_HPP
+#define WHATVER_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T> void swap(T &a, T &b);
+
+template <typename T> T min(T &a, T &b);
+
+template <typename T> T max(T &a, T &b);
+
+#endif
